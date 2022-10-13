2abb504bb4febc16e13616490e5eb170a045e517
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-10-13 15:57:24 +0200

    add snack 1

--- index.php
+++ index.php
@@ -34,12 +34,19 @@ $calendario = [
 for($i=0; $i < count($calendario); $i++){
     $match = $calendario[$i];
 
+    echo $match["partita"];
 }
-
-   echo $calendario[$match];
 ?>
 
+<?= $match["partita"] ?>
 
+<!-- Snack 2
+Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
+
+<?php
+
+
+?>
 
 </body>
 </html>
