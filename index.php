a2738578d04327c71e98051b0cb241dec22973e7
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-10-13 15:44:17 +0200

    create array

--- index.php
+++ index.php
@@ -8,5 +8,38 @@
 </head>
 <body>
     
+<!-- Snack 1
+Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
+Olimpia Milano - Cantù | 55-60
+Snack 2
+Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
+Snack 3
+Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post.
+Qui l’array di esempio: https://www.codepile.net/pile/R2K5d68z
+Snack 4
+Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo.
+Snack 5
+Creare un array contenente qualche alunno di un’ipotetica classe. Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici. Stampare Nome, Cognome e la media dei voti di ogni alunno. -->
+
+<?php
+
+$calendario = [
+    [ "partita" => 'Orlando - Chicago | 55-60'],
+    [ "partita" => 'Denver - New York | 55-60'],
+    [ "partita" => 'Salt Lake City - Los Angeles | 55-60'],
+    [ "partita" => 'Miami - Vancouver | 55-60'],
+];
+
+
+for($i=0; $i < count($calendario); $i++){
+    $match = $calendario[$i];
+
+}
+
+   echo $calendario[$match];
+?>
+
+
+
 </body>
 </html>
